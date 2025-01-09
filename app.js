4dd02b4d3c811a701a44c268da1271b88a3779f0
Franco Ariel Canavese <fcanavese@hotmail.com>
2025-01-09 18:56:28 +0100

    promise_all

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors') // libreria express-async-errors (try/cathc)
 const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
